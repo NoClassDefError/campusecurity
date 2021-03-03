453595975efa79fc9e7045cdde918b9174636d73
DELL <aaa>
2021-03-03 11:36:11 +0800

    #18 编写Dockerfile，为了调试注册中心的监听程序

--- eureka/src/main/java/cn/macswelle/campusecurity/eureka/EurekaApplication.java
+++ eureka/src/main/java/cn/macswelle/campusecurity/eureka/EurekaApplication.java
@@ -3,11 +3,14 @@ package cn.macswelle.campusecurity.eureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableEurekaServer
-@ComponentScan("cn.macswelle.campusecurity.feignapi.deviceManager")
+@EnableFeignClients("cn.macswelle.campusecurity.feignapi.deviceManager")
+@ComponentScan({"cn.macswelle.campusecurity.feignapi.deviceManager",
+        "cn.macswelle.campusecurity.eureka"})
 public class EurekaApplication {
 
     public static void main(String[] args) {
