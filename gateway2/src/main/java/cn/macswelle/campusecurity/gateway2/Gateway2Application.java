049e312c9231eb767d7318f14b3b060799141ea7
DELL <aaa>
2021-03-20 15:44:14 +0800

    #26

--- gateway2/src/main/java/cn/macswelle/campusecurity/gateway2/Gateway2Application.java
+++ gateway2/src/main/java/cn/macswelle/campusecurity/gateway2/Gateway2Application.java
@@ -3,13 +3,8 @@ package cn.macswelle.campusecurity.gateway2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsWebFilter;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-import org.springframework.web.util.pattern.PathPatternParser;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -20,18 +15,18 @@ public class Gateway2Application {
     SpringApplication.run(Gateway2Application.class);
   }
 
-  @Bean
-  public CorsWebFilter corsFilter() {
-    CorsConfiguration config = new CorsConfiguration();
-    // cookie跨域
-    config.setAllowCredentials(Boolean.TRUE);
-    config.addAllowedMethod("*");
-    config.addAllowedOrigin("*");
-    config.addAllowedHeader("*");
-    // 配置前端js允许访问的自定义响应头
-    config.addExposedHeader("setToken");
-    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
-    source.registerCorsConfiguration("/**", config);
-    return new CorsWebFilter(source);
-  }
+//  @Bean
+//  public CorsWebFilter corsFilter() {
+//    CorsConfiguration config = new CorsConfiguration();
+//    // cookie跨域
+//    config.setAllowCredentials(Boolean.TRUE);
+//    config.addAllowedMethod("*");
+//    config.addAllowedOrigin("*");
+//    config.addAllowedHeader("*");
+//    // 配置前端js允许访问的自定义响应头
+//    config.addExposedHeader("setToken");
+//    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+//    source.registerCorsConfiguration("/**", config);
+//    return new CorsWebFilter(source);
+//  }
 }
